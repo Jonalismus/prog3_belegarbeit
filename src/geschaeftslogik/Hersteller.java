239fcd2909a83a19d0545f2003f7c758750c32b0
Jonalismus <103051813+Jonalismus@users.noreply.github.com>
2023-02-20 14:42:19 +0100

    Hersteller, model und model Testklasse angelegt

---
+++ src/geschaeftslogik/Hersteller.java
@@ -0,0 +1,4 @@
+package geschaeftslogik;
+
+public class Hersteller {
+}
---
+++ tst/geschaeftslogik/modelTest.java
@@ -0,0 +1,15 @@
+package geschaeftslogik;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class modelTest {
+
+    // Test überprueft, ob ein Hersteller hinzugefuegt werden kann
+    @Test
+    void herstellerEinfuegen() {
+        model m = new model();
+
+    }
+}
