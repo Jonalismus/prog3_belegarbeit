401b00e771cb52a4c8be66beee6f5df64f163818
Jonalismus <103051813+Jonalismus@users.noreply.github.com>
2023-03-16 16:52:54 +0100

    weiter an net gearbeitet

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -16,7 +16,7 @@ public class GUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getClassLoader().getResource("gui\\startWindow.fxml"))));
+        Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getClassLoader().getResource("gui/startWindow.fxml"))));
         primaryStage.setTitle("Kuchenautomat");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
